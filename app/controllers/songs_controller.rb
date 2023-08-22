940c55cea427ce22d369358308dbbdfccb65c9c8
Jovon Miller <130570205+millerjovon1@users.noreply.github.com>
2023-08-22 16:28:25 +0900

    jsut getting thing

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,9 +7,32 @@ class SongsController < ApplicationController
   end
 
   def new
+    @song = Song.new
+    authorize @song
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+
+    authorize @restaurant
+    if @restaurant.save
+      respond_to do |format|
+        format.html { redirect_to restaurants_path }
+        format.json { render json: {
+          item_html: render_to_string(partial: 'restaurants/card', formats: :html, locals: { restaurant: @restaurant }),
+          form_html: render_to_string(partial: 'restaurants/form', formats: :html, locals: { restaurant: Restaurant.new })
+        } }
+      end
+    else
+      respond_to do |format|
+        format.html {render :new, status: :unprocessable_entity }
+        format.json { render json: {
+          form_html: render_to_string(partial: 'restaurants/form', formats: :html, locals: { restaurant: @restaurant })
+        } }
+      end
+    end
   end
+
 
 end
