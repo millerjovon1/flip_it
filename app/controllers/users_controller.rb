0d0250db856f0cc0c00cfd95ac3edd02ccea66b0
Devi Putri <devi.putri@live.com>
2023-08-29 15:58:59 +0900

    updating the loading page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @crates = @user.crates
+    @crate_song = CrateSong.new
     authorize @user
   end
 
